d5d717a2247f500bbe02bae5ec061f06ceba3f33
Jackz <me@jackz.me>
2019-07-07 18:35:04 -0500

    removed custom commands from git

--- events/guildCreate.js
+++
@@ -1,3 +0,0 @@
-module.exports = (client,guild) => {
-    console.info(`[INFO] Joined guild ${guild.name} with id ${guild.id}`)
-}
--- modules/stats.js
+++
@@ -1,9 +0,0 @@
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
-
-const db = low(new FileSync('./db/stats.json'));
-db.defaults({users:{},global:{imgs:0,stats:0,optout:[]}}).write();
-exports.db = db;
-exports.init = (client) => {
-
-}
